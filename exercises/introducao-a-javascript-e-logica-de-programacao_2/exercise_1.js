3e6f89f6f73c131ddc6f3e2b522d9d04daaa3c14
lmuffato <lmiremuffato@gmail.com>
2021-02-25 16:59:30 -0300

    atualização

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercise_1.js
@@ -0,0 +1,15 @@
+let pizzas = ['pizza0', 'pizza1', 'pizza2'];
+pizzas.push('pizza3')
+pizzas[4] = 'pizza4'
+
+pizzas.length 
+console.log(pizzas)
+
+//Numero aleatório até 60
+Math.random() * 60
+
+//Numero aleatório de 0 a 1
+Math.random()
+
+//Numero teto de 1
+Math.ceil(Math.random)
